97ac617f27d9054bb0b514e7a46f1e172a11cbfa
Becky Kennedy <bk47124@gmail.com>
2018-01-01 08:57:30 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,46 +4,41 @@
 ## Below are a pair of functions that are used to create a special object that 
 ## stores a matrix and caches its inverse.
 
-## This function creates a special "matrix" object that can cache its inverse.
-makeCacheMatrix <- function(x = matrix()) {
+## The makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
 
-# initiate the inverse property
-  my_inv < - NULL 
-# set the value of the matix 
-  set <- function(matrix) {
-    x <<- matrix
+makeCacheMatrix <- function(x = matrix()) {
+  my_inv <- NULL
+#1. set the value of the matrix
+  set <- function(y) {
+    x <<- y
     my_inv <<- NULL
-    
   }
-  
-# get the value of the vector 
+#2. get the value of the matrix
   get <- function() x
-  set_Inv <- function(inverse) my_inv <<- inverse
-  get_Inv <- function() my_inv
+#3. set the value of the inverse
+  set_Inverse <- function(inverse) my_inv <<- inverse
+#4. get the value of the inverse
+  get_Inverse <- function() my_inv
   list(set = set,
        get = get,
-       set_Inv = set_Inv,
-       get_Inv = get_Inv)
+       set_Inverse = set_Inverse,
+       get_Inverse = get_Inverse)
 }
 
 
-### This function computes the inverse of the special "matrix" created by 
-## makeCacheMatrix above. If the inverse has already been calculated (and the 
-## matrix has not changed), then it should retrieve the inverse from the cache.
+
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  my_inv <- x$get_Inv()
-  if (!is.null(inv)) {
+# Return a matrix that's the inverse of 'x'
+  my_inv <- x$get_Inverse()
+
+# 
+  if (!is.null(my_inv)) {
     message("getting cached data")
-    return(my.inv)
+    return(my_inv)
   }
-  mat <- x$get()
-  my_inv <- solve(mat, ...)
-  x$set_Inv(my_inv)
+  my_mat <- x$get()
+  my_inv <- solve(my_mat, ...)
+  x$set_Inverse(my_inv)
   my_inv
 }
-
-
-
-
